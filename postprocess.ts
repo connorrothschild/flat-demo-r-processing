1eb64dece0823e9916fd05c486c9bbc2beefb010
Connor Rothschild <connorrothschild@gmail.com>
2021-05-25 11:04:31 -0500

    install from downloaded r package

--- postprocess.ts
+++ postprocess.ts
@@ -1,8 +1,14 @@
 import 'https://deno.land/x/flat@0.0.10/mod.ts'
 
+const r_dl = Deno.run({
+    cmd: ['wget', 'http://cran.r-project.org/src/contrib/tidyverse_1.3.1.tar.gz']
+});
+
+await r_dl.status();
+
 const r_install = Deno.run({
     // cmd: ['Rscript', '-e', "install.packages('tidyverse')"]
-    cmd: ['sudo', 'R', 'CMD', 'INSTALL', "tidyverse"]
+    cmd: ['sudo', 'R', 'CMD', 'INSTALL', "./tidyverse_1.3.1.tar.gz"]
 });
 
 await r_install.status();
