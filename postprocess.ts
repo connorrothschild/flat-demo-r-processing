a13eebc2b597bcf7f13e056d520061e58ee6c336
Connor Rothschild <connorrothschild@gmail.com>
2021-05-24 22:55:06 -0500

    r cmd batch

--- postprocess.ts
+++ postprocess.ts
@@ -9,7 +9,7 @@ import 'https://deno.land/x/flat@0.0.10/mod.ts'
 
 // Forwards the execution to the python script
 const r_run = Deno.run({
-    cmd: ['Rscript', './clean-data.R'].concat(Deno.args),
+    cmd: ['R CMD BATCH', './clean-data.R']
 });
 
 await r_run.status();
